f746aba360e66991f3b2a613169f83ee01f2e00a
anon-snus <148210808+anon-snus@users.noreply.github.com>
2024-06-27 00:30:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from web3 import Web3
-from web3.middleware import geth_poa_middleware
+
 import csv
 
 def get_private_from_seed(seed: str) -> tuple:
     web3 = Web3()
-    web3.middleware_onion.inject(geth_poa_middleware, layer=0)
     web3.eth.account.enable_unaudited_hdwallet_features()
 
     web3_account = web3.eth.account.from_mnemonic(seed)
@@ -12,6 +11,7 @@ def get_private_from_seed(seed: str) -> tuple:
     private_key = web3_account._private_key.hex()
     address = web3_account.address
     return private_key, address
+
 
 print("choose type: \n 1 - convert from seed \n 2 - convert from private keys \n 0 - exit ")
 a = int(input())
@@ -25,12 +25,12 @@ if a == 1:
         data = []
         for seed in p_keys:
             pk, address = get_private_from_seed(seed)
-            data.append((Web3.to_checksum_address(address), pk))
+            data.append((Web3.to_checksum_address(address), pk, seed))
 
         with open('addresses.csv', 'a+', newline='') as f:
             writer = csv.writer(f)
-            writer.writerow(["Address", "Private Key"])  
-            writer.writerows(data)  
+            writer.writerow(["Address", "Private Key", "Seed"])
+            writer.writerows(data)
 
         print(f'Converted {len(p_keys)} privatekeys')
     except Exception as err:
@@ -46,13 +46,15 @@ elif a == 2:
 
         data = []
         for pk in p_keys:
+            if not pk.startswith('0x'):
+                pk='0x'+pk
             acc = web3.eth.account.from_key(pk)
             data.append((Web3.to_checksum_address(acc.address), pk))
 
         with open('addresses.csv', 'a+', newline='') as f:
             writer = csv.writer(f)
-            writer.writerow(["Address", "Private Key"])  
-            writer.writerows(data)  
+            writer.writerow(["Address", "Private Key"])
+            writer.writerows(data)
 
         print(f'Converted {len(p_keys)} privatekeys')
     except Exception as err:
